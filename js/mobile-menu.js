20b375394075b94ecef2cf52fb51f5807a224562
Sergio Nezhigay <abc1971h@gmail.com>
2023-01-03 20:44:57 +0200

    home page ok

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,14 +2,16 @@
   const mobileMenu = document.querySelector('.js-menu');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const body = document.querySelector('body');
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('open');
     openMenuBtn.classList.toggle('open');
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    body.classList.toggle('scroll-disable');
+    // const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
